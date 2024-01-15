a83dd9ad5c13afd1708c1d5e12a26167dbad6a61
vtp <vtp@ordix.de>
2024-01-15 17:12:58 +0100

    v0.3 Alle Punkte sind erledigt, und alles ist getestet. Ziele und Verlauf Notizendatei ist erzeugt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,33 @@
 package org.example;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Random zufallsGenerator = new Random();
-        int geheimzahl = zufallsGenerator.nextInt(100) + 1;
-        System.out.println("Generierte Geheimzahl: " + geheimzahl);
+        Random random = new Random();
+        int geheimnisZahl = random.nextInt(100) + 1;
+        Scanner scanner = new Scanner(System.in);
+        int versuche = 0;
+        int gerateneZahl;
+
+        System.out.println("Rate die Geheimniszahl zwischen 1 und 100!");
+        System.out.println("Geheimniszahl ist " + geheimnisZahl + " (es ist hier zur Überprüfung)");
+        System.out.println("_______________");
+
+        do {
+            System.out.print("Gib deine Vermutung ein: ");
+            gerateneZahl = scanner.nextInt();
+            versuche++;
+
+            if (gerateneZahl > geheimnisZahl) {
+                System.out.println("Zu hoch! Versuche es nochmal.");
+            } else if (gerateneZahl < geheimnisZahl) {
+                System.out.println("Zu niedrig! Versuche es nochmal.");
+            }
+        } while (gerateneZahl != geheimnisZahl);
+
+        System.out.println("Herzlichen Glückwunsch! Du hast die Zahl in " + versuche + " Versuchen erraten.");
+        scanner.close();
     }
 }
